b173190981a21ba75de2de9b861ab09f75c7f32c
Code Queen <channguyen97@gmail.com>
2023-09-05 14:38:36 -0700

    added pseudocode for javascript file

---
+++ assets/script.js
@@ -0,0 +1,45 @@
+/*
+Global Variables
+
+
+Functions
+
+click event function
+we need a function that targets the event listener for the search bar
+preventdefault
+erase what was previously inputted when clicking the search button
+when users click the search button
+captures input field data from the user
+
+renderdivs function for ingredients
+fetch data from API -- 
+if the user input matches ingredient from the API, the ingredient will be listed
+if the user input does not match an ingredient from the API, the ingredient will not be listed and alert(Please try another search)
+create element-from the input, the unordered list of ingredients generate below
+the listed ingredient will have: a remove button
+
+click event function 
+we need a function that targets the event listener for the search recipe button
+when users click the search recipe button
+captures list of ingredients, feeds this data into next renderdivs function
+
+renderdivs function for recipes
+fetch data from API on recipe name, ingredient list, nutritional fact
+create element-an unordered list of recipes will generate on the right side
+the recipe will display if they include the ingredients the user inputed
+
+dropdown function for recipes
+user clicks on the generated recipe/picture of recipe
+upon click, the recipe will have a dropdown displaying: ingredient list, nutritional facts
+
+
+
+Processes
+an eventlistener for the search ingredient click event
+
+
+an eventlistener for the search recipe click event
+
+
+an eventlistener for the click recipe event-dropdown
+*/
