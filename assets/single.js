a4a61bc194ac2872319985e3db81e822d2f7c361
Brian Lee <brianlee@Brians-MacBook-Pro-2.local>
2023-09-09 18:56:11 -0700

    working fetch call

--- assets/single.js
+++ assets/single.js
@@ -1,10 +1,8 @@
-// ... Previous code ...
 
-// Get meal ID and fetch detailed meal information
 function getMealId() {
     const mealLocal = localStorage.getItem("clickedMeal");
     const extractMealLocal = JSON.parse(mealLocal);
-    const mealId = extractMealLocal[0].idMeal; // Get idMeal property
+    const mealId = extractMealLocal[0].idMeal;
     console.log(mealId);
   
     var requestUrl = "https://www.themealdb.com/api/json/v2/9973533/lookup.php?i=" + mealId;
@@ -21,13 +19,11 @@ function getMealId() {
           const measure = data.meals[0]["strMeasure" + i];
           const ingredient = data.meals[0]["strIngredient" + i];
   
-          // Check if both measure and ingredient are defined and not empty
           if (measure && ingredient) {
             ingredientsPortion.push({ amount: measure, ingredient });
           }
         }
   
-        // Filter the array to remove empty and undefined values
         const filteredArray = ingredientsPortion.filter((value) => {
           return value.amount !== "" && value.ingredient !== "";
         });
@@ -38,66 +34,47 @@ function getMealId() {
             const ingredientTo = filteredArray[i].ingredient;
             const displayBoth = amountTo + (" ") + ingredientTo;
             console.log(displayBoth);
-            console.log(typeof displayBoth);
             finalFilteredIngredientArray.push(displayBoth)
         }
         console.log(finalFilteredIngredientArray);
-        // Call a function here to handle the Edamam API fetch with filteredArray
+        
         fetchEdamam(data.meals[0].strMeal, filteredArray);
-      });
-  }
-  
-  getMealId();
-  
-  // Make fetch call to Edamam with meal title and ingredients
-  function fetchEdamam(mealTitle, ingredients) {
-    const edamamApiEndpoint = "https://api.edamam.com/api/nutrition-details?app_id=429109a1&app_key=397cd538930a1d66e3a85414ec06ce71";
-    const edamamApiKey = "397cd538930a1d66e3a85414ec06ce71";
-  
-    const mealNameLocal = localStorage.getItem("clickedMeal", "strMeal");
-    const extractMealNameLocal = JSON.parse(mealNameLocal);
-    const mealName = extractMealNameLocal[0]['strMeal'];
-    console.log(mealName);
-  
-    const ingredientsArray = [];
+        
+        function fetchEdamam(mealTitle, ingredients) {
+            const edamamApiEndpoint = "https://api.edamam.com/api/nutrition-details?app_id=429109a1&app_key=397cd538930a1d66e3a85414ec06ce71";
+            
+            const mealNameLocal = localStorage.getItem("clickedMeal", "strMeal");
+            const extractMealNameLocal = JSON.parse(mealNameLocal);
+            const mealName = extractMealNameLocal[0]['strMeal'];
+            console.log(mealName);
+            console.log(finalFilteredIngredientArray)
 
-
-  
-    for (let i = 1; i <= 20; i++) {
-        if (i - 1 < ingredients.length) {
-      const amount = ingredients[i - 1].amount; // Access amount from the provided ingredients array
-      const ingredient = ingredients[i - 1].ingredient; // Access ingredient from the provided ingredients array
-  
-      // Check if both amount and ingredient are defined and not empty
-      if (amount && ingredient) {
-        ingredientsArray.push({ amount, ingredient });
-      }
-    }
+            const ingredientsArray = [];
+            
+            fetch(edamamApiEndpoint, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    "title": mealName,
+                    "ingr": finalFilteredIngredientArray,
+                    "yield": "1"
+                })
+            })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+            })
+            .catch(function (error) {
+                console.error('Error:', error);
+                
+            });
+            
+        }
+    });
 }
-  
-    
-        fetch(edamamApiEndpoint, {
-          method: 'POST',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            "title": mealName,
-            "ingr": ingredientsArray,
-            "yield": "1"
-          })
-        })
-          .then(function (response) {
-            return response.json();
-          })
-          .then(function (data) {
-            // Handle the Edamam API response here and display nutrition information on the page
-            // You can create DOM elements to display the nutrition data, similar to how you did for the meal title
-            console.log(data);
-            // Create DOM elements and display nutrition information
-          })
-          .catch(function (error) {
-            console.error('Error:', error);
-          });
-      }
+    getMealId();
